c70208bb7f14abbffe1029c59c297788d5758a97
yj <1069160681@qq.com>
2018-12-23 17:35:02 +0800

    test

--- src/main/java/boot/TestBoot.java
+++ src/main/java/boot/TestBoot.java
@@ -3,7 +3,6 @@ package boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication(scanBasePackages = {"controller","service"})
 public class TestBoot {
     public static void main(String[] args) {
