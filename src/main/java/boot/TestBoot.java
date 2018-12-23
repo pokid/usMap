67a8549c72625c153c23f1dc046740753b585757
yj <1069160681@qq.com>
2018-12-23 17:03:52 +0800

    test1

--- src/main/java/boot/TestBoot.java
+++ src/main/java/boot/TestBoot.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = {"controller","dao","service"})
+@SpringBootApplication(scanBasePackages = {"controller","service"})
 public class TestBoot {
     public static void main(String[] args) {
         SpringApplication.run(TestBoot.class, args);
