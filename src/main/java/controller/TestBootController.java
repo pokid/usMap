50ede2e044aa408a0fe670bce1fd62f24f5f5468
yj <1069160681@qq.com>
2018-12-26 23:57:42 +0800

    demo之页面跳转和js引用方式

--- src/main/java/controller/TestBootController.java
+++ src/main/java/controller/TestBootController.java
@@ -2,8 +2,11 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import service.serviceImpl.TestBootServiceImpl;
 
 @Controller
@@ -12,10 +15,12 @@ public class TestBootController {
     @Autowired
     private TestBootServiceImpl testBootService;
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-
+    //@ResponseBody 传值用ResponseBody，页面跳转不要用
+    //templates为页面模板  static为静态资源
+    //别问我问什么不放在webapp下。sprintboot的坑我已经踩了
+    String home(Model m) {
         System.out.println(testBootService.selectByPrimaryKey(1));
-        return "Hello World!";
+        m.addAttribute("name","你军哥哥");
+        return "test";
     }
 }
---
+++ src/main/resources/templates/test.ftl
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<script type="text/javascript" src="/static/js/test.js">
+
+</script>
+<script type="text/javascript">
+    window.onload = function(){
+        attackEnemy()
+    }
+</script>
+<head>
+    <title>freemark</title>
+</head>
+<body>
+<h1>Hello ${name} from resource freemark!</h1>
+</body>
+</html>
